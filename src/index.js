cacfe4cead982fef47eb92ae71e3d885d7dde052
James Lee <uptownhr@gmail.com>
2017-10-13 23:36:54 -0700

    refactored index.js to call init()

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const logger = require('./lib/logger')
 const app = require('./app')
 
-app.listen(process.env.port || 3000, () => {
-  logger.info('http listening on', process.env.PORT)
+app.init().then(app => {
+  app.listen(process.env.port || 3000, () => {
+    logger.info('http listening on', process.env.PORT)
+  })
 })
